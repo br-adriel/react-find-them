0f6e880b74161c74f21e354ea322cbbd30af70cc
Adriel <adriel.fsantos@outlook.com>
2022-12-26 21:34:57 -0300

    Adiciona componente Header #4

---
+++ src/components/Header/style.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const HeaderTag = styled.header`
+  background: var(--bg-0);
+  padding: 10px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  color: #fff;
+
+  & > div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    h1 {
+      font-family: 'Bungee', cursive;
+      font-size: 2rem;
+      line-height: 0.5rem;
+    }
+
+    div {
+      display: flex;
+      gap: 8px;
+
+      img {
+        width: 30px;
+        height: 30px;
+        border-radius: 5px;
+      }
+    }
+  }
+`;
