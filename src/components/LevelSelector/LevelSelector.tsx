c730ed095ba57793d1e4365eccfe11e965daebec
Adriel <adriel.fsantos@outlook.com>
2022-12-28 19:29:30 -0300

    Altera implementação de LevelSelector #16

--- src/components/LevelSelector/LevelSelector.tsx
+++ src/components/LevelSelector/LevelSelector.tsx
@@ -1,15 +1,23 @@
-import GameContext from '../../contexts/GameContext';
 import { useContext } from 'react';
-import * as S from './style';
+import { useNavigate } from 'react-router-dom';
+import GameContext from '../../contexts/GameContext';
+import { Level } from '../../global/types';
 import LevelCard from '../LevelCard';
+import * as S from './style';
 
 const LevelSelector = () => {
-  const { levels, setSelectedLevel } = useContext(GameContext);
+  const { levels } = useContext(GameContext);
+  const navigate = useNavigate();
+
+  const selectLevel = (l: Level) => {
+    navigate(`/play/${l.id}`);
+  };
+
   return (
     <S.LevelsGrid>
       {levels.map((l) => {
         return (
-          <LevelCard key={l.id} level={l} onClick={() => setSelectedLevel(l)} />
+          <LevelCard key={l.id} level={l} onClick={() => selectLevel(l)} />
         );
       })}
     </S.LevelsGrid>
