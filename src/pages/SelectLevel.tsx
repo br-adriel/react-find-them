f4c542c47c051bf94bba5fdf80b1551fd7a1babb
Adriel <adriel.fsantos@outlook.com>
2022-12-28 15:36:33 -0300

    Adiciona pagina de seleção de  nível #17

---
+++ src/pages/SelectLevel.tsx
@@ -0,0 +1,12 @@
+import Container from '../components/Container';
+import LevelSelector from '../components/LevelSelector';
+
+const SelectLevel = () => {
+  return (
+    <Container>
+      <LevelSelector />
+    </Container>
+  );
+};
+
+export default SelectLevel;
