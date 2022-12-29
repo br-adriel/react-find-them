3806d8d67a9085b767bbf781870930e85fc7aa2a
Adriel <adriel.fsantos@outlook.com>
2022-12-29 17:40:29 -0300

    Torna CharacterBar sticky #16

--- src/components/CharacterBar/style.tsx
+++ src/components/CharacterBar/style.tsx
@@ -3,11 +3,15 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
+  width: calc(100% -20px);
   gap: 10px;
   background: var(--bg-0);
   padding: 10px;
   border-radius: 5px;
   color: #fff;
+  position: sticky;
+  top: 0;
+  margin-bottom: 10px;
 `;
 
 export const CharacterImage = styled.img<{ found: boolean }>`
