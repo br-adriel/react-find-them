c06eee40c75c42cebe593c1fbf88b0eb0e35d2d6
Adriel <adriel.fsantos@outlook.com>
2022-12-30 17:16:48 -0300

    Insere Timer em página PlayLevel #22

--- src/pages/PlayLevel.tsx
+++ src/pages/PlayLevel.tsx
@@ -4,6 +4,8 @@ import CharacterBar from '../components/CharacterBar';
 import GameImage from '../components/GameImage';
 import GameContext from '../contexts/GameContext';
 import Container from '../components/Container';
+import Timer from '../components/Timer';
+import styled from 'styled-components';
 
 const PlayLevel = () => {
   const { levels, setSelectedLevel } = useContext(GameContext);
@@ -17,10 +19,24 @@ const PlayLevel = () => {
 
   return (
     <Container>
-      <CharacterBar />
+      <TopBar>
+        <CharacterBar />
+        <Timer />
+      </TopBar>
       <GameImage />
     </Container>
   );
 };
+
+const TopBar = styled.div`
+  display: flex;
+  width: calc(100% -20px);
+  gap: 10px;
+  position: sticky;
+  top: 0;
+  margin-bottom: 10px;
+  justify-content: space-between;
+  align-items: stretch;
+`;
 
 export default PlayLevel;
