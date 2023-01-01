c6dedc812650f0e953ee8d44531a0680e0b16ba4
Adriel <adriel.fsantos@outlook.com>
2023-01-01 17:35:39 -0300

    Insere Helmet em página NotFound #45

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,15 +1,25 @@
 import { Robot } from 'react-bootstrap-icons';
+import { Helmet } from 'react-helmet-async';
 import styled from 'styled-components';
 
 const NotFound = () => {
   return (
-    <Wrapper>
-      <div>
-        <Robot />
-        <h2>Page not found</h2>
-        <p>Try another URL</p>
-      </div>
-    </Wrapper>
+    <>
+      <Helmet>
+        <title>Page not found</title>
+        <meta
+          name='description'
+          content="The page you're looking for could not be found"
+        />
+      </Helmet>
+      <Wrapper>
+        <div>
+          <Robot />
+          <h2>Page not found</h2>
+          <p>Try another URL</p>
+        </div>
+      </Wrapper>
+    </>
   );
 };
 
