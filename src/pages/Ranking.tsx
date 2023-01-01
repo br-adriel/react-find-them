5066c74562768c65dd9e359df2f5ec9398349896
Adriel <adriel.fsantos@outlook.com>
2022-12-31 23:43:42 -0300

    Insere RankingList em página de Ranking #26

--- src/pages/Ranking.tsx
+++ src/pages/Ranking.tsx
@@ -2,17 +2,25 @@ import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import Container from '../components/Container';
 import RankingLevelSelector from '../components/RankingLevelSelector';
+import RankingList from '../components/RankingList';
 
 const Ranking = () => {
   let { levelId } = useParams();
   if (!levelId) levelId = ' ';
   return (
-    <Wrapper>
-      <RankingLevelSelector currentId={levelId} />
-    </Wrapper>
+    <Container>
+      <Wrapper>
+        <RankingLevelSelector currentId={levelId} />
+        <RankingList levelId={levelId} />
+      </Wrapper>
+    </Container>
   );
 };
 
-const Wrapper = styled(Container)``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
 
 export default Ranking;
